6b08901af499cbb1c95b7e5c4a2886fa48b52c01
p2nts <gabrieltucker260@gmail.com>
2024-02-28 14:47:02 -0800

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import LogoAnimation from './LogoAnimation';
 import Projects from './Projects';
 import AboutMe from './AboutMe';
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import './Contact.css';
 
 function Contact() {
   return (
     <div className="contact">
       <h1>Contact Me</h1>
-      <h4> Reach out to me:</h4>
       <a href="mailto:Gabrieltuckereze@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
       <br />
       <a href="https://www.linkedin.com/in/gabrieltuckereze/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
