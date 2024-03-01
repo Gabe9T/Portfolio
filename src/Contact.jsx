7d13fdcc2aceac3252f69e946215f534fc32260d
gabe <gabrieltucker260@gmail.com>
2024-03-01 13:53:49 -0800

    added icons

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,14 +1,25 @@
 import './Contact.css';
+import emailIcon from './img/email.png';
+import linkedinIcon from './img/linkedin.png'; 
+import githubIcon from './img/github.png'; 
 
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <a href="mailto:Gabrieltuckereze@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
+      <div>
+        <h1>Contact Me</h1>
+      </div>
+      <a href="mailto:Gabrieltuckereze@gmail.com" target="_blank" rel="noopener noreferrer">
+        <img src={emailIcon} alt="Email" />
+      </a>
       <br />
-      <a href="https://www.linkedin.com/in/gabrieltuckereze/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/gabrieltuckereze/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinIcon} alt="LinkedIn" />
+      </a>
       <br />
-      <a href="https://github.com/Gabe9T" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://github.com/Gabe9T" target="_blank" rel="noopener noreferrer">
+        <img src={githubIcon} alt="GitHub" />
+      </a>
     </div>
   );
 }
