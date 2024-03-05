650562072237bd77e4d9889c8023565ac889043d
p2nts <gabrieltucker260@gmail.com>
2024-03-05 15:10:30 -0800

    changed icons

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,25 +1,23 @@
 import './Contact.css';
 import emailIcon from './img/email.png';
-import linkedinIcon from './img/linkedin.png'; 
-import githubIcon from './img/github.png'; 
+import linkedinIcon from './img/linkedin.png';
+import githubIcon from './img/github.png';
 
 function Contact() {
   return (
     <div className="contact">
-      <div>
-        <h1>Contact Me</h1>
+      <h1>Contact Me</h1>
+      <div className="icons">
+        <a href="mailto:Gabrieltuckereze@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src={emailIcon} alt="Email" />
+        </a>
+        <a href="https://www.linkedin.com/in/gabrieltuckereze/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinIcon} alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/Gabe9T" target="_blank" rel="noopener noreferrer">
+          <img src={githubIcon} alt="GitHub" />
+        </a>
       </div>
-      <a href="mailto:Gabrieltuckereze@gmail.com" target="_blank" rel="noopener noreferrer">
-        <img src={emailIcon} alt="Email" />
-      </a>
-      <br />
-      <a href="https://www.linkedin.com/in/gabrieltuckereze/" target="_blank" rel="noopener noreferrer">
-        <img src={linkedinIcon} alt="LinkedIn" />
-      </a>
-      <br />
-      <a href="https://github.com/Gabe9T" target="_blank" rel="noopener noreferrer">
-        <img src={githubIcon} alt="GitHub" />
-      </a>
     </div>
   );
 }
